9a393261e4918bd5e491a3868166121e0f203b15
sakuna63 <saku.ni63@gmail.com>
2013-03-10 23:34:11 +0900

    Add moc

---
+++ client/Topick/src/com/kosenventure/sansan/topick/Account.java
@@ -0,0 +1,13 @@
+package com.kosenventure.sansan.topick;
+
+public class Account {
+
+	String name,state,image;
+	
+	public Account(String name, String state, String image) {
+		this.name = name;
+		this.state = state;
+		this.image = image;
+	}
+
+}
